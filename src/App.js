b0892f6378fa7dff707c334b72ab13c7a990e5d5
roronya <roronya628@gmail.com>
2019-09-29 17:09:47 +0900

    remove

--- src/App.js
+++ src/App.js
@@ -10,17 +10,21 @@ export default () => {
     // Simple one-time requestsではFirestoreに問い合わせている時間の間コネクションを維持してくれずチャンネルが落ちてしまったのでconnectを使っている
     // connectの引数はnameというキーを持ったオブジェクトでなければならない
     // postMessageの引数は任意のオブジェクトを渡してメッセージとしてevent.jsに渡せるが、メッセージによる処理の場合分けは無いので適当に送っている
-    const port = chrome.runtime.connect({ name: 'dreamyfish' });
-    port.postMessage('hello');
-    port.onMessage.addListener((msg) => {
+    const port = chrome.runtime.connect({ name: 'dreamyfish' })
+    port.postMessage('hello')
+    port.onMessage.addListener(msg => {
       setGames(msg.games)
-    });
+    })
   }, [])
   return (
     <div>
       <h3>このゲームに似ているゲーム</h3>
       <Layout>
-        {games ? games.map(game => <Game key={game.id} game={game} />) : <Loading />}
+        {games ? (
+          games.map(game => <Game key={game.id} game={game} />)
+        ) : (
+          <Loading />
+        )}
       </Layout>
     </div>
   )
